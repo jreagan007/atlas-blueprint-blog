d5a4e87490d024e5c51dd2a1a3de7d6c5a4d86a3
Jared <110068184+jreagan007@users.noreply.github.com>
2022-11-24 16:16:39 -0500

    Update preview.js
    replacing/stripping posts slug
    
    import { PostComponent } from './posts/[postSlug]';

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -1,6 +1,6 @@
 import { client } from 'client';
 
-import { PostComponent } from './posts/[postSlug]';
+import { PostComponent } from './[postSlug]';
 import { PageComponent } from './[...pageUri]';
 
 export default function Preview() {
